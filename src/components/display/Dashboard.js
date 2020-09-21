4db5d0109d155bfdf35f7c27b5ce26bdea5b77cb
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-09-21 09:46:16 -0700

    update dashboard

---
+++ src/components/display/Dashboard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import User from './User';
+import Vacations from './Vacations';
+import NavBar from './NavBar'
+
+import {  makeStyles } from '@material-ui/core';
+import LightFlight from '../../assets/flight1.png';
+
+const useStyles = makeStyles(() => ({
+	vacations: {
+		backgroundImage: `url(${LightFlight})`,
+		backgroundSize: 'cover',
+		backgroundRepeat: 'no-repeat',
+		backgroundPosition: 'center',
+		minHeight: '100vh',
+	},
+}));
+
+const Dashboard = () => {
+	const classes = useStyles();
+	return (
+		<>
+			<div className={classes.vacations}>
+				<NavBar />
+				<User />
+				<Vacations />
+			</div>
+		</>
+	);
+};
+
+export default Dashboard;
