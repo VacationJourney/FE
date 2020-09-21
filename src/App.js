0267b954feb2f5cbb430dbdfb35cb6de8e320c2b
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-09-21 07:36:39 -0700

    build out routes for SPA

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+
+import Splash from './components/Display/Splash';
+import Register from './components/Auth/Register';
+import Settings from './components/Display/Settings'
+import Dashboard from './components/Display/Dashboard';
+import OneVacation from './components/Display/OneVacation';
+import UpdateVacation from './components/Display/UpdateVacation';
+import OneDay from './components/Display/OneDay';
+import OneEvent from './components/Display/OneEvent';
+
+import PrivateRoute from './Utils/PrivateRoute';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Switch>
+				<Route path='/register'>
+					<Register />
+				</Route>
+				<Route exact path='/'>
+					<Splash />
+				</Route>
+				<PrivateRoute path='/dashboard'>
+					<Dashboard />
+				</PrivateRoute>
+				<PrivateRoute path='/settings'>
+					<Settings />
+				</PrivateRoute>
+				<PrivateRoute path='/vacationUpdate/:id'>
+					<UpdateVacation />
+				</PrivateRoute>
+				<PrivateRoute path='/vacation/:id'>
+					<OneVacation />
+				</PrivateRoute>
+				<PrivateRoute path='/day/:id'>
+					<OneDay />
+				</PrivateRoute>
+				<PrivateRoute path='/event/:id'>
+					<OneEvent />
+				</PrivateRoute>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
