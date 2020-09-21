5bcd3470ec556e2b3544ea7d8ad4c955331cfceb
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-09-21 09:44:21 -0700

    update paths

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Splash from './components/Display/Splash';
-import Register from './components/Auth/Register';
-import Settings from './components/Display/Settings'
-import Dashboard from './components/Display/Dashboard';
-import OneVacation from './components/Display/OneVacation';
-import UpdateVacation from './components/Display/UpdateVacation';
-import OneDay from './components/Display/OneDay';
-import OneEvent from './components/Display/OneEvent';
+import Splash from './components/display/Splash';
+import Register from './components/auth/Register';
+import Settings from './components/display/Settings'
+import Dashboard from './components/display/Dashboard';
+import OneVacation from './components/display/OneVacation';
+import UpdateVacation from './components/display/UpdateVacation';
+import OneDay from './components/display/OneDay';
+import OneEvent from './components/display/OneEvent';
 
 import PrivateRoute from './utils/PrivateRoute';
 import './App.css';
