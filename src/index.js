2d164af8ca4ebb22b639ec743eb6394ead6b8d9e
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-09-21 07:36:04 -0700

    create index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+
+
+const httpLink = new createHttpLink({
+	uri: process.env.REACT_APP_ENDPOINT || 'http://localhost:4000/'
+});
+
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem('token');
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
+});
+
+const client = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+	onError: ({ networkError, graphQLErrors }) => {
+		console.log('graphQLErrors', graphQLErrors)
+		console.log('networkError', networkError)
+	  }
+});
+
+export const clear = () => {
+    client.cache.reset();
+}
+
+const ApolloApp = () => (
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Router>
+		<React.StrictMode>
+			<ApolloApp />
+		</React.StrictMode>
+	</Router>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
