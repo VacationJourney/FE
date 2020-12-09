600a93f6ea1f016432a82657a37df5236001569d
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-12-09 15:35:38 -0800

    build out marketing styles

--- src/components/display/Splash/Adventure.js
+++ src/components/display/Splash/Adventure.js
@@ -7,12 +7,8 @@ const Adventure = () => {
       <h2>Journey Co$t</h2>
       <p>Creating Vacation Possibilities</p>
       <div className="tagline">
-        <p>A</p>
-        <p>Vacation</p>
-        <p>Budget</p>
-        <p>Planner</p>
+        vacation budget planner
       </div>
-      
       <div className="register">
         <Link className="link" to='/register'>
           Register
--- src/components/display/Splash/Excuses.js
+++ src/components/display/Splash/Excuses.js
@@ -3,7 +3,15 @@ import React from 'react'
 const Excuses = () => {
   return (
     <div id="excuses">
-      <h1>A <br/>VACATION <br/> IS WHAT YOU <br/> TAKE <br/><br/>WHEN <br/>YOU CAN <br/>NO LONGER <br/>TAKE<br/> <br/>WHAT<br/> YOU'VE BEEN <br/>TAKING!</h1>
+      <div className="small">
+        <h1>A <br />VACATION <br /> IS WHAT YOU <br /> TAKE <br /><br />WHEN <br />YOU CAN <br />NO LONGER <br />TAKE<br /> <br />WHAT<br /> YOU'VE BEEN <br />TAKING!</h1>
+        
+      </div>
+      <div className="big">
+        <h1>A VACATION IS WHAT YOU TAKE</h1>
+        <h1>WHEN YOU CAN NO LONGER TAKE</h1>
+        <h1>WHAT YOU'VE BEEN TAKING!</h1>
+      </div>
       <p>Earl Wilson</p>
     </div>
   )
