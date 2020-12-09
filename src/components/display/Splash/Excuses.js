0ea6f7943767226e00424ac899aa2ec2b77948d9
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-12-09 14:17:42 -0800

    style marketing pages

--- src/components/display/Splash/Excuses.js
+++ src/components/display/Splash/Excuses.js
@@ -3,14 +3,8 @@ import React from 'react'
 const Excuses = () => {
   return (
     <div id="excuses">
-      <h1>Journey Cost</h1>
-      <div className="description">
-        <h3 >Too $$$$?</h3>
-        <p className="longing">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut consequuntur fugit accusamus repellendus quam ad facilis vero ut, quos illum sed deleniti voluptatum aspernatur rem eveniet, ex cumque quia inventore?
-        </p>
-        <h3>No Time?</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut similique voluptatibus fuga. Ipsa a, voluptatibus cumque amet molestias possimus exercitationem molestiae necessitatibus rem illo asperiores eveniet magni, ullam explicabo similique!</p>
-      </div>
+      <h1>A <br/>VACATION <br/> IS WHAT YOU <br/> TAKE <br/><br/>WHEN <br/>YOU CAN <br/>NO LONGER <br/>TAKE<br/> <br/>WHAT<br/> YOU'VE BEEN <br/>TAKING!</h1>
+      <p>Earl Wilson</p>
     </div>
   )
 }
