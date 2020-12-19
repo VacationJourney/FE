f9ae2cd24911f06c47d300e74ca800fbde9154ea
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-12-18 17:20:00 -0800

    add calendar styles

--- src/components/Style/CalStyle.js
+++ src/components/Style/CalStyle.js
@@ -8,6 +8,13 @@ export const useStyles = makeStyles((theme) => ({
     boxShadow: '0 3px 3px'
     // border: '1px solid orange'
   },
+  calTop: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  arrows: {
+    fontSize: '2rem'
+  },
   grid: {
     display: 'grid',
     gridTemplateColumns: 'repeat(7, 1fr)',
@@ -24,21 +31,26 @@ export const useStyles = makeStyles((theme) => ({
     margin: '2%',
     height: 'auto',
     fontSize: '1.1rem'
-   
+
   },
   datesGrid: {
     display: 'grid',
     gridTemplateColumns: 'repeat(7, 1fr)',
-    gridRowGap: '3%',
-    height: '25vh',
+    gridRowGap: '2%',
+    height: '28vh',
     background: 'rgb(255,255,255, 0.6)',
   },
-  dates:{
+  monthDates: {
     fontSize: '1.2rem',
     color: '#056099',
   },
-  otherMonth: {
+  otherDates: {
     fontSize: '1.2rem',
     color: 'grey'
+  },
+  trip: {
+    background: 'rgb(5,100,155,0.8)',
+    fontSize: '1.2rem',
+    color: 'white'
   }
 }))
