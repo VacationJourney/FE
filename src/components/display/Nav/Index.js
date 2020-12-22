6e8a01932cd8c495029ccca4f27a5aaf5f372113
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-12-21 16:40:29 -0800

    add logo to component

--- src/components/display/Nav/Index.js
+++ src/components/display/Nav/Index.js
@@ -1,14 +1,20 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 import Menu from './Menu'
-import User from './User'
+// import User from './User'
 import '../../Style/Vacation.css'
+import JCost from '../../../assets/J.png'
 
 const NavBar = () => {
     return (
         <div className='navbar'>
-           <User />
-            <Menu />
+            <Link className='logoAnchor' to="/dashboard">
+                <img src={JCost} className='journeyCostLogo' alt="Journey Co$t Logo"/>
+            </Link>
+            <div className='menu'>
+                {/* <User /> */}
+                <Menu />
+            </div>
         </div>
     )
 }
