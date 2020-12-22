261a82b794ab0800ad756373079bba9bd84b77b7
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-12-21 16:43:40 -0800

    create calendar component for oneVacation

--- src/components/display/OneVacation/Calendar/Index.js
+++ src/components/display/OneVacation/Calendar/Index.js
@@ -1,34 +1,34 @@
 import React, { useState } from 'react'
 import dayjs from 'dayjs'
+import clsx from 'clsx'
 import useDate from '../../../../hooks/useDate'
 import { Paper, Grid, Box, Typography } from '@material-ui/core'
 import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
-import { useStyles } from '../../../Style/CalendarStyle'
+import { useStyles } from '../../../Style/CalendarStyle';
+import OneDay from './OneDay'
 
 
 const Index = ({ dates }) => {
   const classes = useStyles()
-  // const datesLength = dates.length
+  const datesLength = dates.length
   const tripStart = dates[0].date
-  // const tripEnd = dates[datesLength- 1].date
-  // console.log('trip', tripStart, tripEnd)
-  // console.log('dates', dates)
+  const start = dayjs(tripStart).format('YYYY-M-D')
+  const end = dayjs(dates[datesLength - 1].date).format('YYYY-M-D')
   const [date, setDate] = useState(dayjs(tripStart))
-  const [selected, setSelected] = useState(dayjs(date).format('YYYY-MM-DD'))
-  // console.log('date', selected)
+  const [selected, setSelected] = useState(start)
+// console.log('tripStart', date)
   // de-Structure from useDateHook
   const {
     currentDay,
     tripMonth,
     tripYear,
     daysInMonth,
-    firstDateOfMonth,
-    lastDateOfMonth,
     firstWeekday,
     lastWeekday,
     weekDays
   } = useDate(date)
+
 
   // create Arrays for: previousDays
   // Vars for time complexity
@@ -58,16 +58,17 @@ const Index = ({ dates }) => {
   }
   const totalDays = [].concat(prevDays, monthArray, nextDays)
   const tripCal = {}
+
   totalDays.map(day => {
     tripCal[day] = null
     dates.map(trip => {
-      if(trip.date === day){
+
+      if (trip.date === day) {
         tripCal[day] = trip
       }
     })
-    
   })
-  // console.log('tripCal', tripCal, dates)
+
   // fns to navigate months
   const lastMonth = () => {
     setDate(date.subtract(1, 'Month'))
@@ -77,50 +78,75 @@ const Index = ({ dates }) => {
   }
 
   return (
-    <Paper className={ classes.calendar }>
-      <Box className={ classes.calTop }>
-        <ArrowLeftIcon className={ classes.arrows } onClick={ lastMonth } />
-        <Typography variant='h5'>{ date.format('MMMM YYYY') }</Typography>
-        <ArrowRightIcon className={ classes.arrows } onClick={ nextMonth } />
-      </Box>
-      <Grid container className={ classes.grid }>
-        { weekDays.map(day => (
-          <Grid className={ classes.weekdays } item key={ day }>
-            {day }
-          </Grid>
-        )) }
-      </Grid>
-      <Grid container className={ classes.datesGrid }>
+    <>
+      <Paper className={ classes.calendar }>
+        <Box className={ classes.calTop }>
+          <ArrowLeftIcon className={ classes.arrows } onClick={ lastMonth } />
+          <Typography variant='h5'>{ date.format('MMMM YYYY') }</Typography>
+          <ArrowRightIcon className={ classes.arrows } onClick={ nextMonth } />
+        </Box>
+        <Grid container className={ classes.grid }>
+          { weekDays.map(day => (
+            <Grid className={ classes.weekdays } item key={ day }>
+              {day }
+            </Grid>
+          )) }
+        </Grid>
+        <Grid className={ classes.datesGrid }>
+          { totalDays.map(d => {
+            let month = dayjs(d).format('M')
+            let trpMth = (tripMonth + 1).toString()
+            let date = dayjs(d).format('D')
 
+            return (
+              month === trpMth ?
 
-        { totalDays.map(date => {
-          let month = dayjs(date).format('M')
-          let trpMth = (tripMonth + 1).toString()
-          return (
-            month === trpMth ? 
+                tripCal[d] !== null ?
+                  <Grid className={ clsx(classes.trip, {
+                    [classes.tripDate]: selected === d
+                  }) } item onClick={ () => setSelected(d) } key={ d }>
+                    { date }
+                  </Grid> :
+                  <Grid className={ classes.monthDates } item key={ d }>
+                    { date }
+                  </Grid>
+                :
+                tripCal[d] !== null ?
+                  // nest onclick fns to move months
+                  trpMth > month ?
+                    <Grid className={ classes.beyondDate }
+                      onClick={ () => { nextMonth(); setSelected(d) } }
+                      item key={ d }>
+                      { date }
+                    </Grid>
+                    :
+                    <Grid className={ classes.beyondDate }
+                      onClick={ () => { lastMonth(); setSelected(d) } }
+                      item key={ d }>
+                      { date }
+                    </Grid>
+                  :
+                  <Grid className={ classes.otherDates } item key={ d }>
+                    { date }
+                  </Grid>
+            )
+          })
+          }
 
-            tripCal[date] !== null ?
-            <Grid className={ classes.trip } item key={ date }>
-                { dayjs(date).format('D') }
-              </Grid> :
-              <Grid className={ classes.monthDates } item key={ date }>
-                { dayjs(date).format('D') }
-              </Grid>
-              :
-              tripCal[date] !== null ?
-              <Grid className={ classes.trip } item key={ date }>
-                { dayjs(date).format('D') }
-              </Grid> :
-              <Grid className={ classes.otherDates } item key={ date }>
-                { dayjs(date).format('D') }
-              </Grid>
-
-          )
-        })
-        }
-
-      </Grid>
-    </Paper>
+        </Grid>
+      </Paper>
+      <OneDay
+        selected={ selected }
+        setSelected={ setSelected }
+        dates={ dates }
+        date={date}
+        lastMonth={lastMonth}
+        nextMonth={nextMonth}
+        tripCal={tripCal}
+        start={ start }
+        end={ end }
+      />
+    </>
   )
 }
 
