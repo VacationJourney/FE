3260eefa234e18ab2a6bde2951ea142c0b9fa292
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-12-27 17:53:38 -0800

    style for OneDay in component

--- src/components/display/OneVacation/Calendar/OneDay.js
+++ src/components/display/OneVacation/Calendar/OneDay.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import dayjs from 'dayjs'
 import useDate from '../../../../hooks/useDate'
-import { Paper, Typography, Card } from '@material-ui/core'
+import { useMutation } from '@apollo/react-hooks'
+import { UPDATE_DAY_COST, UPDATE_VACATION_COST } from '../../../../graphQl/mutations/vacationM'
+import { GET_ONE_TRIP } from '../../../../graphQl/queries'
+import { Paper, Typography, Card, Box } from '@material-ui/core'
 import { useStyles } from '../../../Style/OneDayStyle'
 import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 import AddBoxIcon from '@material-ui/icons/AddBox';
 
 
-const OneDay = ({ selected, setSelected, dates, date, start, end, lastMonth, nextMonth, tripCal }) => {
+const OneDay = ({ selected, setSelected, trip, date, start, end, lastMonth, nextMonth, tripCal }) => {
   const classes = useStyles()
+  const vacationId = localStorage.getItem('vacationId')
+  // console.log('vacationId', vacationId)
   // de-Structure from useDateHook
   const { firstDateOfMonth, lastDateOfMonth } = useDate(date)
 
@@ -18,11 +23,10 @@ const OneDay = ({ selected, setSelected, dates, date, start, end, lastMonth, nex
   const futureDate = dayjs(selected).add(1, 'Day').format('YYYY-M-D')
   // TripDates Array
   const tripDates = []
-  dates.forEach(d => tripDates.push(d.date))
+  trip.dates.forEach(d => tripDates.push(d.date))
 
   // fns to navigate dates jumping thru months
   const lastDate = () => {
-    // console.log('pastDate', pastDate)
     if (!tripDates.includes(pastDate)) {
       setSelected(start)
     } else if (tripDates.includes(pastDate)) {
@@ -46,7 +50,7 @@ const OneDay = ({ selected, setSelected, dates, date, start, end, lastMonth, nex
       }
     }
   }
-  // console.log('tripCal[selected]', tripCal[selected])
+
   return (
     <Paper className={ classes.date }>
       <div className={ classes.dateTop }>
@@ -58,14 +62,29 @@ const OneDay = ({ selected, setSelected, dates, date, start, end, lastMonth, nex
           onClick={ nextDate }
         />
       </div>
-      <div>
-        <AddBoxIcon />
+      <div className={ classes.eventBox }>
+        <div className={ classes.dayCost }><span>Day Cost</span>
+          <span>${ tripCal[selected].cost }</span>
+        </div>
+
+
+        <AddBoxIcon fontSize="large" />
       </div>
-      {tripCal[selected] && tripCal[selected].events.map((e) => (
-        <Card>{e.title} </Card>
-      ))}
+
+      {tripCal[selected] && tripCal[selected].events.map((e) => {
+
+        return (
+          <Card className={ classes.event } key={ e.id }>
+
+            <div>{ e.title }</div>
+            <div></div>
+            <div>${ e.cost }</div>
+          </Card>
+        )
+      }) }
+
     </Paper>
   )
-}
 
+}
 export default OneDay
