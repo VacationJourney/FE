327660504842692d79bc881c8c68c211e681ab11
Jeremy McWilliams <j2macwilliams@gmail.com>
2021-01-09 07:29:11 -0800

    map thru refs to access singular children components

--- src/components/display/VacationCard.js
+++ src/components/display/VacationCard.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, createRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/react-hooks';
 import { DELETE_VACATION } from '../../graphQl/mutations/vacationM';
@@ -11,41 +11,53 @@ import { useStyles } from '../Style/VacationsStyle'
 
 const VacationCard = ({ data }) => {
 	const classes = useStyles();
-	const deleteTripModal = useRef(null)
+	const tripsLength = data.vacations.length
+	const deleteTripRef = useRef([])
+	// create array of refs 
+	if (deleteTripRef.current.length !== tripsLength) {
+		deleteTripRef.current = Array(tripsLength).fill().map((_, i) => deleteTripRef.current[i] || createRef())
+	}
 	// GraphQL
 	const [deleteVacation] = useMutation(DELETE_VACATION, {
 		refetchQueries: mutationResult => [{ query: GET_VACATIONS }],
 	});
-
+	const deleteTrip = (trip, i) => {
+		console.log('trip', trip)
+		deleteVacation({ variables: { id: trip.id } })
+		deleteTripRef.current[i].current.close()
+	}
 	return (
 		<div className={ classes.root }>
 			<Grid container spacing={ 2 }>
-				{ data.vacations.map(trip => (
-					<Grid item key={ trip.id } xs={ 12 } sm={ 6 } md={ 3 } lg={ 3 }>
+				{ data.vacations.map((trip, i) => (
+					<>
+						<Grid item key={ trip.id } xs={ 12 } sm={ 6 } md={ 3 } lg={ 3 }>
 
-						<Card className={ classes.vacationCard }>
-							<Link className={ classes.vLink } to={ `/vacation/${trip.id}` }>
-								<Typography variant='h5'>
-									{ trip.title }
-								</Typography>
-							</Link>
-							<Button className={ classes.deleteButton } 
-							onClick={ () => deleteTripModal.current.open() }>
-								<DeleteIcon />
-							</Button>
-						</Card>
-						<Modal ref={ deleteTripModal }>
+							<Card className={ classes.vacationCard }>
+								<Link className={ classes.vLink } to={ `/vacation/${trip.id}` }>
+									<Typography variant='h5'>
+										{ trip.title }
+									</Typography>
+								</Link>
+								<Button className={ classes.deleteButton }
+									onClick={ () => deleteTripRef.current[i].current.open()}>
+									<DeleteIcon />
+								</Button>
+							</Card>
+
+						</Grid>
+						<Modal ref={ deleteTripRef.current[i] }>
 							<Typography variant='h6'>
 								Confirm deleting trip?
       				</Typography>
 							<Button
 								className={ classes.deleteButtonRed }
-								onClick={ () => (deleteVacation({ variables: { id: trip.id } }), deleteTripModal.current.close()) }>
+								onClick={ () => deleteTrip(trip, i) }>
 								<DeleteIcon />
 								<ListItemText primary={ trip.title } />
 							</Button>
 						</Modal>
-					</Grid>
+					</>
 				)) }
 			</Grid>
 		</div>
