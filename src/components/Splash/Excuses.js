4a82f30e1c62339880d404590a2eb1ec8f072be3
Jeremy McWilliams <j2macwilliams@gmail.com>
2021-01-13 06:21:47 -0800

    change file structure architecture

---
+++ src/components/Splash/Excuses.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import { Link } from 'react-router-dom';
+const Excuses = () => {
+  return (
+    <div id="excuses">
+      <div className="small">
+        <h1>A <br />VACATION <br /> IS WHAT YOU <br /> TAKE <br /><br />WHEN <br />YOU CAN <br />NO LONGER <br />TAKE<br /> <br />WHAT<br /> YOU'VE BEEN <br />TAKING!</h1>
+        
+      </div>
+      <div className="big">
+        <h1>A VACATION IS WHAT YOU TAKE</h1>
+        <h1>WHEN YOU CAN NO LONGER TAKE</h1>
+        <h1>WHAT YOU'VE BEEN TAKING!</h1>
+      </div>
+      <p>Earl Wilson</p>
+    </div>
+  )
+}
+
+export default Excuses
---
+++ src/components/Splash/Index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Splash from './Splash'
+import Excuses from './Excuses'
+import Adventure from './Adventure'
+import './Splash.css'
+
+const Index = () => {
+  return (
+    <div>
+      <Splash />
+      <Excuses />
+      <Adventure />
+    </div>
+  )
+}
+
+export default Index
+
