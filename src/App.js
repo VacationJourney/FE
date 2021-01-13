af73e9223b0a624fefaa39d57bb6675d43ea8c2f
Jeremy McWilliams <j2macwilliams@gmail.com>
2021-01-12 17:43:26 -0800

    update app.js routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-
-import Splash from './components/display/Splash/Index';
-import Register from './components/auth/Register';
-import Profile from './components/display/Profile'
-import Dashboard from './components/display/Dashboard';
-import OneVacation from './components/display/OneVacation/Index.js';
-import UpdateVacation from './components/display/UpdateVacation';
-
 import PrivateRoute from './utils/PrivateRoute';
+
+import Splash from './components/Splash/Index';
+import Dashboard from './components/Authorize/Index';
+
 import './App.css';
 
 function App() {
 	return (
 		<div className='App'>
 			<Switch>
-				<Route path='/register'>
-					<Register />
-				</Route>
 				<Route exact path='/'>
 					<Splash />
 				</Route>
-				<PrivateRoute path='/dashboard'>
+				<PrivateRoute path='/vacations'>
 					<Dashboard />
 				</PrivateRoute>
-				<PrivateRoute path='/profile'>
-					<Profile />
-				</PrivateRoute>
-				<PrivateRoute path='/vacationUpdate/:id'>
-					<UpdateVacation />
-				</PrivateRoute>
-				<PrivateRoute path='/vacation/:id'>
-					<OneVacation />
-				</PrivateRoute>
-				
 			</Switch>
 		</div>
 	);
